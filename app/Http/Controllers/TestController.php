6ace6f81bfce9f91ec60887cafd02eeb28a4407d
Ning <qi_zhengning@163.com>
2019-01-12 17:20:46 +0800

    .idea

---
+++ app/Http/Controllers/TestController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 齐正宁
+ * Date: 2019/1/12
+ * Time: 17:12
+ */
+namespace App\Http\Controllers;
+
+class TestController extends Controller{
+
+    public function index(){
+        echo 'Test-index';
+    }
+}
